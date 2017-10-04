649d9d57cba77f361a248b0d674726504534cb24
naderscode <naderscode@gmail.com>
2017-10-03 20:02:03 -0400

    create extract module

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 var http = require('http');
 var fs = require('fs');
-var path = require('path');
+//var path = require('path');
+var extract = require('./extract');
 
 var server = http.createServer(function(req, res){
   console.log('Responding to a request. ');
-  //res.end('<h1>Hello World</h1>');
-  var url = req.url;
-  console.log(url);
-  var fileName = 'test.html';
-  if(url.length > 1){
-    filename = url.substring(1);
 
-  }
-  console.log(fileName);
-  var filePath = path.resolve(__dirname, 'app', fileName);
+  var filePath = extract(req.url);
   console.log(filePath);
   fs.readFile(filePath, function(err, data){
     res.end(data);
