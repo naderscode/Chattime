d8ae64fa703d7411b1330780c56addcf000dc274
naderscode <naderscode@gmail.com>
2017-10-03 23:27:47 -0400

    created chat server functionality

--- websockets-server.js
+++ websockets-server.js
@@ -4,14 +4,22 @@ var port = 3001;
 var ws = new WebSocketServer({
   port: port
 });
+var messages = [];
 
 console.log('websockets server started');
 
 ws.on('connection', function(socket){
   console.log('client connection establihed');
 
+  messages.forEach(function(msg){
+    socket.send(msg);
+  });
+
   socket.on('message', function(data){
     console.log('message received: ' + data);
-    socket.send(data);
+    messages.push(data);
+    ws.clients.forEach(function (clientSocket){
+      clientSocket.send(data);
+    });
   });
 });
