e6a19d66c4fe86eb5df27625ca0fd38b65676708
naderscode <naderscode@gmail.com>
2017-10-06 20:11:24 -0400

    create event handlers

--- app/scripts/src/app.js
+++ app/scripts/src/app.js
@@ -4,6 +4,13 @@ class ChatApp {
   constructor(){
     console.log('ChatApp constructor');
     socket.init('ws://localhost:8000');
+    socket.registerOpenHandler(() => {
+      let message = new ChatMessage({ message: 'Hello there!' });
+      socket.sendMessage(message.serialize());
+    });
+    socket.registerMessageHandler((data) => {
+      console.log(data);
+    });
   }
 }
 
@@ -11,11 +18,11 @@ class ChatMessage {
   constructor({
     message: m,
     user: u='Nader',
-    timestamp: t=(newDate()).getTime()
+    timestamp: t=(new Date()).getTime()
   }) {
-    this.message=message;
-    this.user=user;
-    this.timestamp=timestamp;
+    this.message = message;
+    this.user = user;
+    this.timestamp = timestamp;
   }
   serialize(){
     return {
--- app/scripts/src/ws-client.js
+++ app/scripts/src/ws-client.js
@@ -6,6 +6,28 @@ function init(url){
 
 }
 
+function registerOpenHandler(handlerFunction){
+  socket.onopen = () => {
+    console.log('open');
+    handlerFunction();
+  };
+}
+
+function registerMessageHandler(){
+  socket.onmessage = (e) => {
+    console.log('message', e.data);
+    let data = JSON.parse(e.data);
+    handlerFunction(data);
+  };
+}
+
+function sendMessage(payload){
+  socket.send(JSON.stringify(payload));
+}
+
 export default {
   init,
+  registerOpenHandler,
+  registerMessageHandler,
+  sendMessage
 }
