e164f41ea02ba9e9115a731c1f0b88e9663e6ff6
naderscode <naderscode@gmail.com>
2017-10-07 17:17:38 -0400

    create ChatForm class

--- app/scripts/src/app.js
+++ app/scripts/src/app.js
@@ -1,11 +1,20 @@
-import socket from './ws-client'
+import socket from './ws-client';
+import {ChatForm} from './dom';
+
+const FORM_SELECTOR = '[data-chat="chat-form"]';
+const INPUT_SELECTOR = '[data-chat="message-input"]';
 
 class ChatApp {
   constructor(){
+    this.ChatForm = new ChatForm(FORM_SELECTOR, INPUT_SELECTOR);
+
     socket.init('ws://localhost:8000');
     socket.registerOpenHandler(() => {
-      let message = new ChatMessage({ message: 'Hello there'});
-      socket.sendMessage(message.serialize());
+      //let message = new ChatMessage({ message: 'Hello there'});
+      this.ChatForm.init((data) => {
+        let message = new ChatMessage(data);
+        socket.sendMessage(message.serialize());
+      });
     });
     socket.registerMessageHandler((data) => {
       console.log(data);
